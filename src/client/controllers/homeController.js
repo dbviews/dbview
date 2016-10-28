3b37bc5cdda305a8c80d6f9e1a8b168f6ef08301
David Wilson <w.dave.w@gmail.com>
2016-10-27 19:07:02 -0700

    feat: add initial login form

---
+++ src/client/controllers/homeController.js
@@ -0,0 +1,26 @@
+angular
+  .module('Dbview.HomeController', ['ngRoute'])
+  .controller('HomeController', ['$scope', '$http', HomeController])
+
+function HomeController($scope, $http) {
+  $scope.creds = {};
+  $scope.dialects = ['Postgres', 'MySQL'],
+  $scope.post = function () {
+    $http({
+      method: 'POST',
+      url: '/requestDB',
+       headers: {
+        'Content-Type': 'application/json'
+      },
+      data: $scope.creds
+    })
+    .then((response) => {
+      console.log(response);
+    });
+    console.log('sending request with', $scope.creds);
+  }
+
+
+}
+
+
