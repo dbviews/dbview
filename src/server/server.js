7b766ef9c7d467a213781cf347fe6ca256196d00
David Wilson <w.dave.w@gmail.com>
2016-10-27 19:35:12 -0700

    Resolve server js merge conflict

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//extra middleware
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}), bodyParser.json());
 app.use(express.static(path.join(__dirname, '../../node_modules/')));
 app.use(express.static(path.join(__dirname, '../client/')));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+app.post('/requestDB', userCtrl.sendDB);
+
+app.listen(3000, ()=> console.log('listening on port 3000'));
