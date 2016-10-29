c578951de1fc81bf4fa42ff6752c22ea20c17f09
David Wilson <w.dave.w@gmail.com>
2016-10-28 19:49:52 -0700

    add a filter to table view

--- src/client/controllers/tableController.js
+++ src/client/controllers/tableController.js
@@ -6,6 +6,30 @@ function tableController($scope, tableService, $stateParams, dbService) {
   $scope.name = $stateParams.tablename;
 
   // reference the data that will be rendered to a table format
-  $scope.dataToRender = tableService.getData($stateParams.tablename);
-}
+  $scope.gridData = {
+    data: tableService.getData($stateParams.tablename),
+    enableFiltering: true,
+  }
+
+
+    // $scope.query = function () {
+    //   $http({
+    //     method: 'POST',
+    //     url: '/requestTable',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data: { creds: dbService.creds, table }
+    //   })
+    //     .then((response) => {
+
+    //       // add this table to the nav bar
+    //       activateTable($scope, table, tableService);
+
+    //       // save the data in table service
+    //       tableService.addTableData(table, response.data);
+    //     });
+    // }
+
+  }
 
