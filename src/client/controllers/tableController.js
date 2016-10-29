fa573783f72b3fee88f86f6400b98095f78d1f17
David Wilson <w.dave.w@gmail.com>
2016-10-28 20:07:30 -0700

    Add initial query form

--- src/client/controllers/tableController.js
+++ src/client/controllers/tableController.js
@@ -1,35 +1,41 @@
 angular
   .module('Dbview.TableController', ['ui.router'])
-  .controller('TableController', ['$scope', 'tableService', '$stateParams', 'dbService', tableController])
+  .controller('TableController', ['$scope', 'tableService', '$stateParams', 'dbService', '$http', tableController])
 
-function tableController($scope, tableService, $stateParams, dbService) {
+function tableController($scope, tableService, $stateParams, dbService, $http) {
   $scope.name = $stateParams.tablename;
+  $scope.dataToDisplay = tableService.getData($scope.name);
+  $scope.query = '';
 
   // reference the data that will be rendered to a table format
   $scope.gridData = {
-    data: tableService.getData($stateParams.tablename),
+    data: tableService.getData($scope.name),
     enableFiltering: true,
   }
 
-
-    // $scope.query = function () {
-    //   $http({
-    //     method: 'POST',
-    //     url: '/requestTable',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     },
-    //     data: { creds: dbService.creds, table }
-    //   })
-    //     .then((response) => {
-
-    //       // add this table to the nav bar
-    //       activateTable($scope, table, tableService);
-
-    //       // save the data in table service
-    //       tableService.addTableData(table, response.data);
-    //     });
-    // }
-
+  $scope.refreshTable = function (tableData) {
+    tableService.addTableData($scope.name, tableData)
+    $scope.dataToDisplay = tableService.getData($scope.name);
   }
+
+  $scope.executeQuery = function () {
+    $http({
+      method: 'POST',
+      url: '/requestTable',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: { creds: dbService.creds, command: $scope.query }
+    })
+      .then((response) => {
+
+        // add this table to the nav bar
+        activateTable($scope, table, tableService);
+
+        // save the data in table service
+        tableService.addTableData(table, response.data);
+      });
+  }
+
+}
 
